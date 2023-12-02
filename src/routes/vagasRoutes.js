cd457b22979c3b2fc855ead3c3530334be47a211
Jadson Luciano Ferreira da Silva <jadson.silva@selfitacademias.com.br>
2023-12-02 14:41:08 -0300

    back

---
+++ src/routes/vagasRoutes.js
@@ -0,0 +1,14 @@
+// Arquivo de rotas da aplicação
+import express from "express"
+import vagasController from '../controllers/vagasControllers.js'
+
+//metodo especifico  para gestão de rotas
+const routes = express.Router();
+
+//passar a rota e controller como parametro
+routes.get("/futbol", vagasController.listarfutbol);
+routes.get("/futbol/:id", vagasController.listarfutbolPorId);
+routes.post("/futbol", vagasController.cadastrarfutbol);
+routes.put("/Futbol/:id", vagasController.cadastrarfutbol);
+
+export default routes;
